066c5cbe0cc4e47d6dd7d9b7b705d1f5be2f0666
AAEvstefeev <alexanderevstefeev@MacBook-Pro-Alexander-Evstefeev.local>
2016-03-19 16:55:01 +0800

    kz

--- src/main/java/com/bane/app/config/WebConfig.java
+++ src/main/java/com/bane/app/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/we/**").addResourceLocations("/resources/");
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
 
     @Override
